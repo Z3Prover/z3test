757289febf2d3be45c7bf85cb99afed72930d39f
Leonardo de Moura <leonardo@microsoft.com>
2012-12-06 08:26:16 -0800

    fixed bug
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- scripts/bigtest.py
+++ scripts/bigtest.py
@@ -13,8 +13,8 @@ def bigtest(b="unstable"):
             util.buildz3(branch=b, everything=True, clean=True, debug=d,  java=True, static=True, jobs=config.NUMJOBS, clang=c)
             util.testz3py(branch=b, debug=b, clang=c)
             util.testjavaex(branch=b, debug=b, clang=c)
-            util.testz3ex('cpp_example')
-            util.testz3ex('c_example')    
+            util.testz3ex('cpp_example', branch=b, debug=b, clang=c)
+            util.testz3ex('c_example', branch=b, debug=b, clang=c)    
 
 if __name__ == "__main__":
     bigtest()
