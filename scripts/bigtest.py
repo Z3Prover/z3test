9691037c045d096c14dcad239a0500f1b379afb3
Leonardo de Moura <leonardo@microsoft.com>
2012-12-13 11:15:24 -0800

    added mcsat to big build
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- scripts/bigtest.py
+++ scripts/bigtest.py
@@ -6,24 +6,26 @@ import config
 import datetime
 import sys
 
-def bigtest(b="unstable"):
+def bigtest():
     print "BUILD DATE:", datetime.date.today()
     sys.stdout.flush()
     # Build everything:
     #   - using clang and g++
     #   - release and debug modes
-    for d in [True, False]:
-        for c in [True, False]:
-            util.buildz3(branch=b, everything=True, clean=True, debug=d,  java=True, static=True, jobs=config.NUMJOBS, clang=c)
-            util.testz3py(branch=b, debug=d, clang=c)
-            util.testjavaex(branch=b, debug=d, clang=c)
-            util.testz3ex('cpp_example', branch=b, debug=d, clang=c)
-            util.testz3ex('c_example', branch=b, debug=d, clang=c)
-            util.test_benchmarks_using_latest('regressions/smt2', branch=b, debug=d, clang=c)
-            if d:
-                util.test_benchmarks_using_latest('regressions/smt2-debug', branch=b, debug=d, clang=c)
-            util.test_benchmarks_using_latest('regressions/smt2-extra', branch=b, debug=d, clang=c)
-            util.test_pyscripts_using_latest('regressions/python', branch=b, debug=d, clang=c)
+    for b in ['unstable']:
+        for d in [True, False]:
+            for c in [True, False]:
+                util.buildz3(branch=b, everything=True, clean=True, debug=d,  java=True, static=True, jobs=config.NUMJOBS, clang=c)
+                util.testz3py(branch=b, debug=d, clang=c)
+                util.testjavaex(branch=b, debug=d, clang=c)
+                util.testz3ex('cpp_example', branch=b, debug=d, clang=c)
+                util.testz3ex('c_example', branch=b, debug=d, clang=c)
+                util.test_benchmarks_using_latest('regressions/smt2', branch=b, debug=d, clang=c)
+                if d:
+                    util.test_benchmarks_using_latest('regressions/smt2-debug', branch=b, debug=d, clang=c)
+                    util.test_benchmarks_using_latest('regressions/smt2-extra', branch=b, debug=d, clang=c)
+                    util.test_pyscripts_using_latest('regressions/python', branch=b, debug=d, clang=c)
+    util.buildz3(branch='mcsat', everything=True, clean=True, debug=True,  java=False, static=False, jobs=config.NUMJOBS, clang=False)
 
 if __name__ == "__main__":
     bigtest()
