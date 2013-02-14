77c574a8469c047ba2f16e5560ce78d4d830e767
Christoph M. Wintersteiger <cwinter@microsoft.com>
2013-02-14 12:36:12 +0000

    Added new build names for codeplex uploads.
    Signed-off-by: Christoph M. Wintersteiger <cwinter@microsoft.com>

--- ClusterExperiment/CodeplexUpload/Program.cs
+++ ClusterExperiment/CodeplexUpload/Program.cs
@@ -39,6 +39,34 @@ namespace CodeplexUpload
             return res;
         }
 
+        static void mkRelease(ReleaseService rs, string name, string description)
+        {
+            try
+            {
+                rs.CreateARelease("Z3", name, description, null, ReleaseStatus.Planned, true, false);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        static void upload(ReleaseService rs, FileInfo fi, string release, string checkfile)
+        {
+            if (isNewer(checkfile, fi.FullName))
+            {
+                ReleaseFile f = new ReleaseFile();
+                f.Name = fi.Name;
+                f.MimeType = "application/octet-stream";
+                f.FileName = fi.Name;
+                f.FileType = ReleaseFileType.RuntimeBinary;
+                f.FileData = File.ReadAllBytes(fi.FullName);
+
+                List<ReleaseFile> files = new List<ReleaseFile>();
+                files.Add(f);
+                rs.UploadReleaseFiles("Z3", release, files, f.Name);
+            }
+        }
+
         static void Main(string[] args)
         {
             Configuration config = new Configuration("config.xml");
@@ -47,62 +75,52 @@ namespace CodeplexUpload
             rs.Credentials = new NetworkCredential(config.codeplex_username, config.codeplex_password);
             rs.Timeout = 600000;
 
-            string releaseWinX86 = "Nightly unstable Windows x86 build";
-            string releaseWinX64 = "Nightly unstable Windows x64 build";
-
-            try
-            {
-                rs.CreateARelease("Z3", releaseWinX86, "Automatic nightly Windows x32 build of the unstable branch.", null, ReleaseStatus.Planned, true, false);
-            }
-            catch (Exception)
-            {
-            }
-
-            try
-            {
-                rs.CreateARelease("Z3", releaseWinX64, "Automatic nightly Windows x64 build of the unstable branch.", null, ReleaseStatus.Planned, true, false);
-            }
-            catch (Exception)
-            {
-            }
+            string releaseWinX86 = "Windows x86 unstable";
+            string releaseWinX64 = "Windows x64 unstable";
+            string releaseDebianX64 = "Debian x64 unstable";
+            string releaseFreeBSDX64 = "FreeBSD x64 unstable";
+            string releaseOSX = "OSX x64 unstable";
+            string releaseUbuntuX64 = "Ubuntu x64 unstable";
+            string releaseUbuntuX86 = "Ubuntu x86 unstable";
+            
 
             DirectoryInfo di = new DirectoryInfo(config.package_dir);
             foreach (FileInfo fi in di.GetFiles("*"))
             {
-                if (fi.Name.Contains("z3-win"))
+                if (fi.Name.Contains("-x86-win"))
                 {
-                    if (fi.Name.Contains("-x86"))
-                    {
-                        if (isNewer("last_cp_win_x86", fi.FullName))
-                        {
-                            ReleaseFile f = new ReleaseFile();
-                            f.Name = fi.Name;
-                            f.MimeType = "application/octet-stream";
-                            f.FileName = fi.Name;
-                            f.FileType = ReleaseFileType.RuntimeBinary;
-                            f.FileData = File.ReadAllBytes(fi.FullName);
-
-                            List<ReleaseFile> files = new List<ReleaseFile>();
-                            files.Add(f);
-                            rs.UploadReleaseFiles("Z3", releaseWinX86, files, f.Name);
-                        }
-                    }
-                    else if (fi.Name.Contains("-x64"))
-                    {
-                        if (isNewer("last_cp_win_x64", fi.FullName))
-                        {
-                            ReleaseFile f = new ReleaseFile();
-                            f.Name = fi.Name;
-                            f.MimeType = "application/octet-stream";
-                            f.FileName = fi.Name;
-                            f.FileType = ReleaseFileType.RuntimeBinary;
-                            f.FileData = File.ReadAllBytes(fi.FullName);
-
-                            List<ReleaseFile> files = new List<ReleaseFile>();
-                            files.Add(f);
-                            rs.UploadReleaseFiles("Z3", releaseWinX64, files, f.Name);
-                        }
-                    }
+                    mkRelease(rs, releaseWinX86, "Automatic nightly Windows x32 build of the unstable branch.");
+                    upload(rs, fi, releaseWinX86, "last_cp_win_x86");
+                }
+                else if (fi.Name.Contains("-x64-win"))
+                {
+                    mkRelease(rs, releaseWinX64, "Automatic nightly Windows x64 build of the unstable branch.");
+                    upload(rs, fi, releaseWinX64, "last_cp_win_x64");
+                }
+                else if (fi.Name.Contains("-x64-debian"))
+                {
+                    mkRelease(rs, releaseDebianX64, "Automatic nightly Debian x64 build of the unstable branch.");
+                    upload(rs, fi, releaseDebianX64, "last_cp_debian_x64");
+                }
+                else if (fi.Name.Contains("-x64-freebsd"))
+                {
+                    mkRelease(rs, releaseFreeBSDX64, "Automatic nightly FreeBSD x64 build of the unstable branch.");
+                    upload(rs, fi, releaseFreeBSDX64, "last_cp_freebsd_x64");
+                }
+                else if (fi.Name.Contains("-x64-ubuntu"))
+                {
+                    mkRelease(rs, releaseUbuntuX64, "Automatic nightly Ubuntu x64 build of the unstable branch.");
+                    upload(rs, fi, releaseUbuntuX64, "last_cp_ubuntu_x64");
+                }
+                else if (fi.Name.Contains("-x86-ubuntu"))
+                {
+                    mkRelease(rs, releaseUbuntuX86, "Automatic nightly Ubuntu x86 build of the unstable branch.");
+                    upload(rs, fi, releaseUbuntuX86, "last_cp_ubuntu_x86");
+                }
+                else if (fi.Name.Contains("-x64-osx"))
+                {
+                    mkRelease(rs, releaseOSX, "Automatic nightly OSX x64 build of the unstable branch.");
+                    upload(rs, fi, releaseOSX, "last_cp_osx_x64");
                 }
             }
         }
