7c0ceacf8bc5eb726050f749fc01967914a34c5b
Nikolaj Bjorner <nbjorner@microsoft.com>
2013-06-11 12:26:25 -0700

    remove TPTP from test build
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/bigtest.py
+++ scripts/bigtest.py
@@ -28,7 +28,7 @@ def bigtest():
             if util.is_windows():
                 util.test_cs_using_latest('regressions/cs', branch=b, debug=d, clang=False)
     util.buildz3(branch='mcsat', everything=True, clean=True, debug=True,  java=False, static=False, jobs=config.NUMJOBS, clang=False)
-    util.buildz3(branch='tptp', everything=True, clean=True, debug=True,  java=False, static=False, jobs=config.NUMJOBS, clang=False)
+#   util.buildz3(branch='tptp', everything=True, clean=True, debug=True,  java=False, static=False, jobs=config.NUMJOBS, clang=False)
     util.test_pyscripts_using_latest('regressions/mcsat', branch='mcsat', debug=True, clang=False)
     util.test_benchmarks_using_latest('regressions/mcsat_smt2', branch='mcsat', debug=True, clang=False)
 
