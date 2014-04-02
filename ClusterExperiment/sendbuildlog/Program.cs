85c4d192ff94b5d570adbda15699ebf3cb5b59c8
Christoph M. Wintersteiger <cwinter@microsoft.com>
2014-04-02 17:37:07 +0100

    added sendbuildlog script
    Signed-off-by: Christoph M. Wintersteiger <cwinter@microsoft.com>

---
+++ ClusterExperiment/sendbuildlog/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Z3Data;
+
+namespace sendbuildlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration c = new Configuration("config.xml");
+            
+            Sendmail.Send(c.developers,
+                          "Build failed at Windows (x86/x64)", 
+                          "The build failed; please check the attached logfile for errors.", 
+                          c.z3_drop_dir + "\\build.log");
+        }
+    }
+}
