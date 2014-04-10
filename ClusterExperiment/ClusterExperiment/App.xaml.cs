866c74e3d6d6b361c23ab76da8c6f174bae0f094
Christoph M. Wintersteiger <cwinter@microsoft.com>
2014-04-10 14:54:05 +0100

    bugfix

--- ClusterExperiment/ClusterExperiment/App.xaml.cs
+++ ClusterExperiment/ClusterExperiment/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace ClusterExperiment
 {
@@ -12,5 +13,14 @@ namespace ClusterExperiment
   /// </summary>
   public partial class App : Application
   {
+      const string userRoot = "HKEY_CURRENT_USER";
+      const string subkey = "ClusterExperiments";
+      const string keyName = userRoot + "\\" + subkey;
+
+      protected override void OnStartup(StartupEventArgs e)
+      {
+          base.OnStartup(e);
+          RegistryKey rk = Registry.CurrentUser.CreateSubKey(subkey);
+      }
   }
 }
