f93e1aa19c8f6ae672e1e63bc2d3dc5a2948a264
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-05-08 15:47:07 +0100

    making regression test resilient to returning ast-vector
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- regressions/python/sorts.py
+++ regressions/python/sorts.py
@@ -7,6 +7,8 @@ Fruit2 = DeclareSort('Fruit')
 assert(Fruit == Fruit2)
 scriptStr = "(declare-sort Fruit) (declare-const a Fruit) (declare-const b Fruit) (assert (= a b))\n"
 assertions = parse_smt2_string(scriptStr)
+if isinstance(assertions, AstVector):
+   assertions = assertions[0]
 assert(assertions.arg(0).sort() == Fruit)
 assert(Fruit.kind() == Z3_UNINTERPRETED_SORT)
 assert(assertions.arg(0).sort().kind() == Z3_UNINTERPRETED_SORT)
