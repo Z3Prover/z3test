b1e72e4fd48b62d3b8c0b5dc42d13223e4c79b5c
Andrei Lascu <andrei.lascu10@imperial.ac.uk>
2021-09-13 14:45:08 +0100

    Update coverage test timeout

--- scripts/test_coverage_tests.py
+++ scripts/test_coverage_tests.py
@@ -1,5 +1,5 @@
 from util import *
 import sys
 # Usage: z3-install-dir benchmark-dir
-test_cpp(sys.argv[1], sys.argv[2], timeout_duration=60.0)
+test_cpp(sys.argv[1], sys.argv[2], timeout_duration=1800.0)
 exit(0)
